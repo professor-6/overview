20c2b118bf711fbd0e456d4e2b0b2203e42085c3
sydneycasey <casey.sydney@gmail.com>
2019-03-29 15:56:18 -0700

    handling get requests for all data and for dynamic data

--- server/index.js
+++ server/index.js
@@ -1,13 +1,28 @@
 const express = require('express');
 const app = express();
-const data = require('../database/data.js')
+const db = require('../database/index.js');
+const mysql = require('mysql');
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('AAYYYYYYYY!');
+// dynamic endpoint -> /api/restaurants/14 renders data for restaurant 14.
+app.get(`/api/restaurants/:id`, (req, res) => {
+  let id = req.params.id;
+  db.getDataForId(id, (err, results) => {
+    if (err) {
+      console.log('error getting data in server', err);
+    }
+    res.json(results);
+  })
 });
 
-console.log(data.reviews())
+app.get('/api/restaurants', (req, res) => {
+  db.getAllRestaurants((err, results) => {
+    if (err) {
+      console.log('server error getting all data', err);
+    }
+    res.json(results);
+  });
+});
 
 app.listen(port, () => {
   console.log(`app listening on port ${port}`)
