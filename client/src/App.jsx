e9ecc9c983c0853e5e347c91d8f1596dee4889aa
sydneycasey <casey.sydney@gmail.com>
2019-04-03 19:52:28 -0700

    added tests for server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -45,3 +45,5 @@ class App extends React.Component {
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
+
+export default App;
--- client/src/components/Styles.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const overviewStyle = {
-  border: 'solid-black'
-}
