1c91e74d034f14f7968905d79ab6b93df0f5b874
Chagil Guiab <chagilguiab@gmail.com>
2019-04-20 16:19:47 -0700

    removes comments and unused code from generate_records.js

--- database/ten-mill/generate_records.js
+++ database/ten-mill/generate_records.js
@@ -26,35 +26,11 @@ const generate1000Records = () => {
   return records;
 }
 
-const generateOneMill = () => {
-  let oneMill = [];
-  for (let j = 0; j < 1000; j++) {
-    oneMill = oneMill.concat(generate1000Records());
-  }
-  return oneMill;
-}
-
-//==========writeOneMill in JSON=================
-// const writeOneMill = () => {
-//   console.log(`You've hit Promise #${count}`)
-//   return new Promise ((res, rej) => {
-//     fs.writeFile(`records${count}.json`, generateOneMill(), (err) => {
-//       if (err) {
-//         rej(err);
-//       } else {
-//         count++;
-//         res(generateOneMill());
-//       }
-//     })
-//   })
-// }
-//=================writeOneMill in CSV================
 const writeOneMill = () => {
   console.log(`You've hit Promise # ${count}`);
 
   return new Promise ((res, rej) => {
     let writer = csvWriter();
-    // let oneMill = generateOneMill();
     writer.on('error', (err) => {rej(err);});
     writer.on('end', () => res(generate1000Records()));
     writer.pipe(fs.createWriteStream(`record-${count}.csv`));
@@ -67,11 +43,9 @@ const writeOneMill = () => {
 
     writer.end();
     count++;
-    console.log('nice jobbo');
   })
 }
 
-// =====writeTenMill with Promises===========
 let count = 1;
 const writeTenMill = () => {
   writeOneMill()
@@ -86,32 +60,4 @@ const writeTenMill = () => {
   .then (writeOneMill)
 }
 
-//=======writeTenMill Iteratively===========
-// const writeTenMill = () => {
-//   for (let i = 0; i < 10; i++) {
-//     writer.pipe(fs.createWriteStream(`record-${i}.csv`));
-//     generateOneMill().forEach((record) => {
-//       writer.write(record);
-//     });
-//     writer.end();
-//   }
-// }
-
-// writeTenMill();
-
-console.log(data.names.length, data.names);
-// writeOneMill();
-
-// =========CSV: write a single record===========
-// writer.write({
-  //   id: i,
-  //   name: data.names[i],
-  //   description: getDescription(),
-  //   rating: getRating(),
-  //   reviews: getReviews(),
-  //   maxPrice: getMaxPrice(),
-  //   foodType: getFoodType(),
-  //   tag1: getTag1(),
-  //   tag2: getTag2(),
-  //   tag3: getTag3()
-  // })
+writeTenMill();
