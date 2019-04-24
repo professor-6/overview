159321f8a65875d925d57c41a59bd0ff6e805682
Chagil Guiab <chagilguiab@gmail.com>
2019-04-24 11:54:38 -0700

    adds pg-schema.sql file to seed psql database

---
+++ pg-schema.sql
@@ -0,0 +1,35 @@
+DROP TABLE IF EXISTS open_table_reviews;
+
+
+CREATE TABLE open_table_reviews(
+  id SERIAL,
+  name VARCHAR(50),
+  description text,
+  rating decimal,
+  reviews decimal,
+  max_price decimal,
+  food_type VARCHAR(25),
+  tag1 VARCHAR(25),
+  tag2 VARCHAR(25),
+  tag3 VARCHAR(25)
+);
+
+\COPY open_table_reviews (name, description, rating, reviews, max_price, food_type, tag1, tag2, tag3) FROM '/home/chagil/service_sydney/database/ten-mill/record-1.csv' WITH DELIMITER ',' CSV Header
+
+\COPY open_table_reviews (name, description, rating, reviews, max_price, food_type, tag1, tag2, tag3) FROM '/home/chagil/service_sydney/database/ten-mill/record-2.csv' WITH DELIMITER ',' CSV Header
+
+\COPY open_table_reviews (name, description, rating, reviews, max_price, food_type, tag1, tag2, tag3) FROM '/home/chagil/service_sydney/database/ten-mill/record-3.csv' WITH DELIMITER ',' CSV Header
+
+\COPY open_table_reviews (name, description, rating, reviews, max_price, food_type, tag1, tag2, tag3) FROM '/home/chagil/service_sydney/database/ten-mill/record-4.csv' WITH DELIMITER ',' CSV Header
+
+\COPY open_table_reviews (name, description, rating, reviews, max_price, food_type, tag1, tag2, tag3) FROM '/home/chagil/service_sydney/database/ten-mill/record-5.csv' WITH DELIMITER ',' CSV Header
+
+\COPY open_table_reviews (name, description, rating, reviews, max_price, food_type, tag1, tag2, tag3) FROM '/home/chagil/service_sydney/database/ten-mill/record-6.csv' WITH DELIMITER ',' CSV Header
+
+\COPY open_table_reviews (name, description, rating, reviews, max_price, food_type, tag1, tag2, tag3) FROM '/home/chagil/service_sydney/database/ten-mill/record-7.csv' WITH DELIMITER ',' CSV Header
+
+\COPY open_table_reviews (name, description, rating, reviews, max_price, food_type, tag1, tag2, tag3) FROM '/home/chagil/service_sydney/database/ten-mill/record-8.csv' WITH DELIMITER ',' CSV Header
+
+\COPY open_table_reviews (name, description, rating, reviews, max_price, food_type, tag1, tag2, tag3) FROM '/home/chagil/service_sydney/database/ten-mill/record-9.csv' WITH DELIMITER ',' CSV Header
+
+\COPY open_table_reviews (name, description, rating, reviews, max_price, food_type, tag1, tag2, tag3) FROM '/home/chagil/service_sydney/database/ten-mill/record-10.csv' WITH DELIMITER ',' CSV Header
