38233a286c5809313eec23cbb5b0ae73c4817408
Chagil Guiab <chagilguiab@gmail.com>
2019-04-26 21:07:35 -0700

    adds deleteById function to postgres/index.js

--- database/postgres/index.js
+++ database/postgres/index.js
@@ -4,8 +4,53 @@ const sequelize = new Sequelize('open_table', 'postgres', 'hrr37', {
   dialect: 'postgres'
 });
 
+let Review = sequelize.define(`open_table_reviews`, {
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true
+  },
+  name: {
+    type: Sequelize.STRING
+  },
+  description: {
+    type: Sequelize.STRING
+  },
+  rating: {
+    type: Sequelize.DECIMAL
+  },
+  reviews: {
+    type: Sequelize.DECIMAL
+  },
+  max_price: {
+    type: Sequelize.DECIMAL
+  },
+  food_type: {
+    type: Sequelize.STRING
+  },
+  tag1: {
+    type: Sequelize.STRING
+  },
+  tag2: {
+    type: Sequelize.STRING
+  },
+  tag3: {
+    type: Sequelize.STRING
+  },
+  isdeleted: {
+    type: Sequelize.BOOLEAN
+  }
+});
+
 exports.getDataForId = (id, callback) => {
   let queryString = `SELECT * FROM open_table_reviews WHERE id = ${id}`;
   sequelize.query(queryString, {type: sequelize.QueryTypes.SELECT})
   .then(record => {callback(record)});
+}
+
+exports.deleteById = (id, callback) => {
+  let queryString = `UPDATE open_table_reviews SET isdeleted = true WHERE id = ${id}`;
+  sequelize.query(queryString)
+  .then(() => {
+    exports.getDataForId(id, callback);
+  })
 }
