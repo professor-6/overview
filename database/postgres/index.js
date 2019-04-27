756792af696c3f542009ba1ba8564a9ffa785d2a
Chagil Guiab <chagilguiab@gmail.com>
2019-04-26 17:19:42 -0700

    adds postgres/idnex.js file to config Sequelize with postgres and getDataForId function

---
+++ database/postgres/index.js
@@ -0,0 +1,12 @@
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('open_table', 'postgres', 'hrr37', {
+  host: 'localhost',
+  dialect: 'postgres'
+});
+const Conn = new Sequelize("postgres://")
+
+exports.getDataForId = (id, callback) => {
+  let queryString = `SELECT * FROM open_table_reviews WHERE id = ${id}`;
+  sequelize.query(queryString, {type: sequelize.QueryTypes.SELECT})
+  .then(record => {callback(record)});
+}
